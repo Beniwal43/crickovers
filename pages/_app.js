c7b509246e69b79e72eabb19969f09fa35679430
Chetan Raj <91714355+Beniwal43@users.noreply.github.com>
2024-07-18 01:41:50 +0530

    second commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-before-interactive-script-outside-document */
 import "@/styles/globals.css";
 import "@/styles/animate.css";
 import "@/styles/bootstrap.css";
@@ -16,89 +17,89 @@ import Script from "next/script";
 export default function App({ Component, pageProps }) {
   return (
     <React.Fragment>
-      <Header/>
+      <Header />
       <Component {...pageProps} />;
       <Footer />
       <Script
-          src="/assets/js/vendor/jquery.js"
-          strategy="lazyOnload"
-          onLoad={() => console.log(`Script loaded correctly.`)}
-        />
-        <Script
-          src="/assets/js/bootstrap-bundle.js"
-          strategy="lazyOnload"
-          onLoad={() => console.log(`Script loaded correctly.`)}
-        />
-        <Script
-          src="/assets/js/swiper-bundle.js"
-          strategy="lazyOnload"
-          onLoad={() => console.log(`Script loaded correctly.`)}
-        />
-        <Script
-          src="/assets/js/jquery-appear.js"
-          strategy="lazyOnload"
-          onLoad={() => console.log(`Script loaded correctly.`)}
-        />
-        <Script
-          src="/assets/js/jquery-knob.js"
-          strategy="lazyOnload"
-          onLoad={() => console.log(`Script loaded correctly.`)}
-        />
-        <Script
-          src="/assets/js/magnific-popup.js"
-          strategy="lazyOnload"
-          onLoad={() => console.log(`Script loaded correctly.`)}
-        />
-        <Script
-          src="/assets/js/nice-select.js"
-          strategy="lazyOnload"
-          onLoad={() => console.log(`Script loaded correctly.`)}
-        />
-        <Script
-          src="/assets/js/purecounter.js"
-          strategy="lazyOnload"
-          onLoad={() => console.log(`Script loaded correctly.`)}
-        />
-        <Script
-          src="/assets/js/wow.js"
-          strategy="lazyOnload"
-          onLoad={() => console.log(`Script loaded correctly.`)}
-        />
-        <Script
-          src="/assets/js/range-slider.js"
-          strategy="lazyOnload"
-          onLoad={() => console.log(`Script loaded correctly.`)}
-        />
-        <Script
-          src="/assets/js/imagesloaded-pkgd.js"
-          strategy="lazyOnload"
-          onLoad={() => console.log(`Script loaded correctly.`)}
-        />
-        <Script
-          src="/assets/js/ajax-form.js"
-          strategy="lazyOnload"
-          onLoad={() => console.log(`Script loaded correctly.`)}
-        />
-        <Script
-          src="/assets/js/email-decode.min.js"
-          strategy="lazyOnload"
-          onLoad={() => console.log(`Script loaded correctly.`)}
-        />
-        <Script
-          src="/assets/js/jquery.validate.min.js"
-          strategy="lazyOnload"
-          onLoad={() => console.log(`Script loaded correctly.`)}
-        />
-        <Script
-          src="/assets/js/custom.js"
-          strategy="lazyOnload"
-          onLoad={() => console.log(`Script loaded correctly.`)}
-        />
-        <Script
-          src="/assets/js/main.js"
-          strategy="lazyOnload"
-          onLoad={() => console.log(`Script loaded correctly.`)}
-        />
+        src="/assets/js/vendor/jquery.js"
+        strategy="beforeInteractive"
+        onLoad={() => console.log(`Script loaded correctly.`)}
+      />
+      <Script
+        src="/assets/js/bootstrap-bundle.js"
+        strategy="lazyOnload"
+        onLoad={() => console.log(`Script loaded correctly.`)}
+      />
+      <Script
+        src="/assets/js/swiper-bundle.js"
+        strategy="lazyOnload"
+        onLoad={() => console.log(`Script loaded correctly.`)}
+      />
+      <Script
+        src="/assets/js/jquery-appear.js"
+        strategy="lazyOnload"
+        onLoad={() => console.log(`Script loaded correctly.`)}
+      />
+      <Script
+        src="/assets/js/jquery-knob.js"
+        strategy="lazyOnload"
+        onLoad={() => console.log(`Script loaded correctly.`)}
+      />
+      <Script
+        src="/assets/js/magnific-popup.js"
+        strategy="lazyOnload"
+        onLoad={() => console.log(`Script loaded correctly.`)}
+      />
+      <Script
+        src="/assets/js/nice-select.js"
+        strategy="lazyOnload"
+        onLoad={() => console.log(`Script loaded correctly.`)}
+      />
+      <Script
+        src="/assets/js/purecounter.js"
+        strategy="lazyOnload"
+        onLoad={() => console.log(`Script loaded correctly.`)}
+      />
+      <Script
+        src="/assets/js/wow.js"
+        strategy="lazyOnload"
+        onLoad={() => console.log(`Script loaded correctly.`)}
+      />
+      <Script
+        src="/assets/js/range-slider.js"
+        strategy="lazyOnload"
+        onLoad={() => console.log(`Script loaded correctly.`)}
+      />
+      <Script
+        src="/assets/js/imagesloaded-pkgd.js"
+        strategy="lazyOnload"
+        onLoad={() => console.log(`Script loaded correctly.`)}
+      />
+      <Script
+        src="/assets/js/ajax-form.js"
+        strategy="lazyOnload"
+        onLoad={() => console.log(`Script loaded correctly.`)}
+      />
+      <Script
+        src="/assets/js/email-decode.min.js"
+        strategy="lazyOnload"
+        onLoad={() => console.log(`Script loaded correctly.`)}
+      />
+      <Script
+        src="/assets/js/jquery.validate.min.js"
+        strategy="lazyOnload"
+        onLoad={() => console.log(`Script loaded correctly.`)}
+      />
+      <Script
+        src="/assets/js/custom.js"
+        strategy="lazyOnload"
+        onLoad={() => console.log(`Script loaded correctly.`)}
+      />
+      <Script
+        src="/assets/js/main.js"
+        strategy="lazyOnload"
+        onLoad={() => console.log(`Script loaded correctly.`)}
+      />
     </React.Fragment>
   );
 }
